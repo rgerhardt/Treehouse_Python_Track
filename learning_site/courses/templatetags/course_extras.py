c59e190b94081bf419d2bd3df3aa290888b0e723
Ricardo <ric.gerhardt@gmail.com>
2016-10-27 16:50:31 -0200

    Added courses nav tag

--- learning_site/courses/templatetags/course_extras.py
+++ learning_site/courses/templatetags/course_extras.py
@@ -7,3 +7,11 @@ register = template.Library()
 @register.simple_tag
 def newest_course():
     return Course.objects.latest('created_at')
+
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    courses = Course.objects.all()
+    return {'courses': courses}
+
+
