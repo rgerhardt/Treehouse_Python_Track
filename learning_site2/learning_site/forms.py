82693cf544f4cc8b52af5378abfbfb2b662a5a2e
Ricardo <ric.gerhardt@gmail.com>
2016-10-28 17:50:35 -0200

    Added honeypot field

--- learning_site2/learning_site/forms.py
+++ learning_site2/learning_site/forms.py
@@ -6,3 +6,15 @@ class SuggestionFrom(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(required=False,
+                               widget=forms.HiddenInput,
+                               label='Leave Empty')
+
+    def clean_honeypot(self):
+        honeypot = self.cleaned_data['honeypot']
+        if len(honeypot):
+            raise forms.ValidationError(
+                'honeypot should be left empty'
+            )
+        return honeypot
+
