a1882b1866ca668738decb35a581703c5e02223d
Ricardo <ric.gerhardt@gmail.com>
2016-10-28 18:08:08 -0200

    Added verify email field

--- learning_site2/learning_site/forms.py
+++ learning_site2/learning_site/forms.py
@@ -10,12 +10,19 @@ class SuggestionFrom(forms.Form):
 
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(help_text='Please verify your email address')
     suggestion = forms.CharField(widget=forms.Textarea)
     honeypot = forms.CharField(required=False,
                                widget=forms.HiddenInput,
                                label='Leave Empty',
                                validators=[must_be_empty])
 
+    def clean(self):
+        cleaned_data = super(SuggestionFrom, self).clean()
+        email = cleaned_data.get('email')
+        verify = cleaned_data.get('verify_email')
 
+        if email != verify:
+            raise forms.ValidationError('You need to enter the same email in both fields')
 
 
