c10e4a2846c10e427e55ba664d417db738f6cb51
Ricardo <ric.gerhardt@gmail.com>
2016-11-04 20:42:37 -0200

    Only published courses can be exhibited

--- learning_site2/courses/views.py
+++ learning_site2/courses/views.py
@@ -10,12 +10,12 @@ from . import models
 
 
 def course_list(request):
-    courses = models.Course.objects.all()
+    courses = models.Course.objects.filter(published=True)
     return render(request, 'courses/course_list.html', {'courses': courses})
 
 
 def course_detail(request, pk):
-    course = get_object_or_404(models.Course, pk=pk)
+    course = get_object_or_404(models.Course, pk=pk, published=True)
     steps = sorted(chain(course.text_set.all(), course.quiz_set.all()),
                    key=lambda step: step.order)
 
@@ -26,18 +26,18 @@ def course_detail(request, pk):
 
 
 def text_detail(request, course_pk, step_pk):
-    step = get_object_or_404(models.Text, course_id=course_pk, pk=step_pk)
+    step = get_object_or_404(models.Text, course_id=course_pk, pk=step_pk, published=True)
     return render(request, 'courses/step_detail.html', {'step': step})
 
 
 def quiz_detail(request, course_pk, step_pk):
-    step = get_object_or_404(models.Quiz, course_id=course_pk, pk=step_pk)
+    step = get_object_or_404(models.Quiz, course_id=course_pk, pk=step_pk, published=True)
     return render(request, 'courses/quiz_detail.html', {'step': step})
 
 
 @login_required
 def quiz_create(request, course_pk):
-    course = get_object_or_404(models.Course, pk=course_pk)
+    course = get_object_or_404(models.Course, pk=course_pk, published=True)
     form = forms.QuizForm()
 
     if request.method == 'POST':
@@ -53,7 +53,7 @@ def quiz_create(request, course_pk):
 
 @login_required
 def quiz_edit(request, course_pk, quiz_pk):
-    quiz = get_object_or_404(models.Quiz, pk=quiz_pk, course_id=course_pk)
+    quiz = get_object_or_404(models.Quiz, pk=quiz_pk, course_id=course_pk, published=True)
     form = forms.QuizForm(instance=quiz)
 
     if request.method == 'POST':
@@ -163,11 +163,11 @@ def answer_form(request, question_pk):
 
 
 def courses_by_teacher(request, teacher):
-    courses = models.Course.objects.filter(teacher__username=teacher)
+    courses = models.Course.objects.filter(teacher__username=teacher, published=True)
     return render(request, 'courses/course_list.html', {'courses': courses})
 
 
 def search(request):
     term = request.GET.get('q')
-    courses = models.Course.objects.filter(title__icontains=term)
+    courses = models.Course.objects.filter(title__icontains=term, published=True)
     return render(request, 'courses/course_list.html', {'courses': courses})
