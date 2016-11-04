1a7915589a3cdd2e8b6642ee90ba4ce83c7c3c36
Ricardo <ric.gerhardt@gmail.com>
2016-11-04 16:09:16 -0200

    Add new_question template, view and url

--- learning_site2/courses/urls.py
+++ learning_site2/courses/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'(?P<course_pk>\d+)/q(?P<step_pk>\d+)/$', views.quiz_detail, name='quiz'),
     url(r'(?P<course_pk>\d+)/create_quiz/$', views.quiz_create, name='create_quiz'),
     url(r'(?P<course_pk>\d+)/edit_quiz/(?P<quiz_pk>\d+)/$', views.quiz_edit, name='edit_quiz'),
+    url(r'(?P<quiz_pk>\d+)/create_question/(?P<question_type>mc|tf)/$',
+        views.create_question, name='create_question'),
     url(r'(?P<pk>\d+)/$', views.course_detail, name='detail'),
 ]
