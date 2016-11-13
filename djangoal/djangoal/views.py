d83f34bc50e3d897fa62b96ae0279743b0fa174b
Ricardo <ric.gerhardt@gmail.com>
2016-11-13 14:24:12 -0200

    Add hello world page

--- djangoal/djangoal/views.py
+++ djangoal/djangoal/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+class HelloView(View):
+
+    def get(self, request):
+        return HttpResponse("Hello world!!")
