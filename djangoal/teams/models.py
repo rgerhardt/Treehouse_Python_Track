f7a4452b815a889890bf3d3f436ee1690fa4b72d
Ricardo <ric.gerhardt@gmail.com>
2016-11-13 15:30:26 -0200

    Add create update delete class base views to team

--- djangoal/teams/models.py
+++ djangoal/teams/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
 
 POSITIONS = (
@@ -31,6 +32,8 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('teams:detail', kwargs={"pk": self.pk})
 
 class Player(models.Model):
     name = models.CharField(max_length=255)
--- djangoal/teams/views.py
+++ djangoal/teams/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . import models
 
 
@@ -10,3 +11,18 @@ class TeamListView(ListView):
 
 class TeamDetailView(DetailView):
     model = models.Team
+
+
+class TeamCreateView(CreateView):
+    fields = ("name", "practice_location", "coach")
+    model =  models.Team
+
+
+class TeamUpdateView(UpdateView):
+    fields = ("name", "practice_location", "coach")
+    model =  models.Team
+
+
+class TeamDeleteView(DeleteView):
+    model = models.Team
+    success_url = reverse_lazy("teams:list")
