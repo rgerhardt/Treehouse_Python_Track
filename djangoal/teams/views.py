a8a11b60ef362149e2959c39b5b5e3d499b55ec6
Ricardo <ric.gerhardt@gmail.com>
2016-11-13 15:02:43 -0200

    Replace default views by classe base views

--- djangoal/teams/views.py
+++ djangoal/teams/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView, DetailView
 from . import models
 
 
-def team_list(request):
-    teams = models.Team.objects.all()
-    return render(request, 'teams/team_list.html', {'teams': teams})
+class TeamListView(ListView):
+    model = models.Team
+    context_object_name = "teams"
 
 
-def team_detail(request, pk):
-    team = get_object_or_404(models.Team, pk=pk)
-    return render(request, 'teams/team_detail.html', {'team': team})
+class TeamDetailView(DetailView):
+    model = models.Team
